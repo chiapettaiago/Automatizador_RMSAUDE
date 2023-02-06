e40cbe176371feed00575735ad2bf3c9051bcd68
Iago Chiapetta <iagochiapetta@gmail.com>
2023-02-06 16:09:27 -0300

    Commit dia 06 de fevereiro de 2023
    Versão de testes de desenvolvimento

--- automacao.py
+++ automacao.py
@@ -14,7 +14,11 @@ from tkinter import ttk
 import locale
 import cv2
 import numpy as np
-import logging
+import openai
+import win32gui
+import os
+
+openai.api_key = "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"
 
 dsn = cx_Oracle.makedsn('10.1.0.137', 1521, 'dbfesocons')
 user = 'rm'
@@ -113,6 +117,7 @@ def automacao():
     py.click(x, y)
     x, y = py.locateCenterOnScreen('itens/controlerecebimentos.png')
     py.click(x, y)
+    time.sleep(5)
     title = "Controle de Recebimentos (RDESKAMB.UNIFESO.LAN)"
     try:
         window = pygetwindow.getWindowsWithTitle(title)[0]
@@ -301,13 +306,12 @@ def automacao():
 
     pesquisa = py.locateCenterOnScreen('itens/pesquisa.png')
     py.click(pesquisa)
-            
+                
+    fatura = str(entradaFatura.get())
     time.sleep(5)
-            
-    fatura = entradaFatura.get()
-    print(fatura)
     screenshot = pyautogui.screenshot()
-    text = ocr.image_to_data(screenshot, output_type='dict', lang='eng')
+    custom_config = r'--oem 1 --psm 11 tessedit_char_whitelist=1234567890'
+    text = ocr.image_to_data(screenshot, output_type='dict', lang='por', config=custom_config)
     for i in range(len(text['text'])):
         if text['text'][i] == fatura:
             x, y, w, h = text['left'][i], text['top'][i], text['width'][i], text['height'][i]
@@ -315,7 +319,137 @@ def automacao():
             py.click(screen_x, screen_y)
             break
     else:
-        print("Texto não encontrado.")
+        print("Texto não encontrado")
+        i < 0
+        while i < 10:
+            rolar = py.locateCenterOnScreen('itens/rolar.png')
+            py.click(rolar)
+            custom_config = r'--oem 1 --psm 11 tessedit_char_whitelist=1234567890'
+            text = ocr.image_to_data(thresh, output_type='dict', lang='por', config=custom_config)
+            i += 1
+            continue
+
+
+    x, y = py.locateCenterOnScreen('itens/contas.png')
+    py.click(x, y)
+
+    value = int(entradaFatura.get())
+    valuePeriodoMes = entradaPeriodoMes.get()
+    valuePeriodoAno = entradaPeriodoAno.get()
+    valorCompetencia = str(valuePeriodoMes) + '/' + str(valuePeriodoAno)
+
+    #Consulta
+    contador = 0
+    while True:
+        sql = "SELECT 'CONTA INICIAL' TIPO_CONTA,F.SIGLA,A.DATAENTRADA,A.DATASAIDA,B.NUMEROREMESSA REMESSA, to_char(G.DATAGERACAO, 'MM/YYYY') GERACAO,E.PRONTUARIO,E.CODPACIENTE,A.CODATENDIMENTO,E.NOMEPACIENTE PACIENTE,B.IDUNIDFAT,A.CODCOMPRADOR, A.SEQUENCIALCONTA FROM SZATENDIMENTO A INNER JOIN SZPARCIALATEND B ON  A.CODCOLIGADA = B.CODCOLIGADA     AND A.NUMEROCONTA = B.NUMEROCONTA  AND A.SEQUENCIALCONTA = B.SEQUENCIALCONTA INNER JOIN SZPACIENTE E ON  A.CODCOLIGADA = E.CODCOLIGADA  AND A.CODPACIENTE = E.CODPACIENTE INNER JOIN SZCADGERAL F ON A.CODCOLIGADA = F.CODCOLIGADA  AND B.CODCONVENIO = F.CODGERAL INNER JOIN SZFATURACONVENIO G ON A.CODCOLIGADA = G.CODCOLIGADA  AND B.CODCONVENIO = G.CODCOMPRADOR AND B.CODCOLIGADA = G.CODCOLIGADA  AND B.NUMEROREMESSA = G.NUMEROREMESSA WHERE A.CODCOLIGADA = 1 and  to_char(G.DATAGERACAO, 'MM/YYYY')= '"+str(valorCompetencia)+"' and g.numeroremessa = "+str(value)+" and g.codcomprador = "+str(codPlano)+" and B.IDUNIDFAT = "+str(codHospital)+" AND A.CODCOMPRADOR NOT IN (494, 25, 3087) UNION ALL SELECT 'CONTA DE RECURSO' TIPO_CONTA, F.SIGLA, A.DATAENTRADA, A.DATASAIDA, B.NUMEROREMESSA  REMESSA, to_char(G.DATAGERACAO, 'MM/YYYY') GERACAO, E.PRONTUARIO, E.CODPACIENTE, A.CODATENDIMENTO, E.NOMEPACIENTE  PACIENTE, B.IDUNIDFAT, A.CODCOMPRADOR, A.SEQUENCIALCONTA FROM SZATENDIMENTOREC A INNER JOIN SZPARCIALATEND B ON  A.CODCOLIGADA = B.CODCOLIGADA AND A.NUMEROCONTA = B.NUMEROCONTA AND A.SEQUENCIALCONTA = B.SEQUENCIALCONTA INNER JOIN SZPACIENTE E ON  A.CODCOLIGADA = E.CODCOLIGADA  AND A.CODPACIENTE = E.CODPACIENTE INNER JOIN SZCADGERAL F ON A.CODCOLIGADA = F.CODCOLIGADA  AND B.CODCONVENIO = F.CODGERAL INNER JOIN SZFATURACONVENIO G ON A.CODCOLIGADA = G.CODCOLIGADA  AND B.CODCONVENIO = G.CODCOMPRADOR  AND B.CODCOLIGADA = G.CODCOLIGADA  AND B.NUMEROREMESSA = G.NUMEROREMESSA WHERE A.CODCOLIGADA = 1 AND B.STATUS = 'F' and  to_char(G.DATAGERACAO, 'MM/YYYY')= '"+str(valorCompetencia)+"' and g.numeroremessa = "+str(value)+" and g.codcomprador = "+str(codPlano)+" and B.IDUNIDFAT = "+str(codHospital)+" AND G.STATUSPROTOCOLO = 'F' AND A.CODCOMPRADOR NOT IN (494, 25, 3087) ORDER BY PACIENTE"
+        cursor.execute(sql)
+        contaRecurso = cursor.fetchall()[contador]
+        numeroConta = str(contaRecurso[12])
+        tipo_conta = contaRecurso[0]
+        print(tipo_conta, numeroConta, contador)
+        sql2 = "SELECT COUNT(PACIENTE) FROM (SELECT 'CONTA INICIAL' TIPO_CONTA,F.SIGLA,A.DATAENTRADA,A.DATASAIDA,B.NUMEROREMESSA REMESSA, to_char(G.DATAGERACAO, 'MM/YYYY') GERACAO,E.PRONTUARIO,E.CODPACIENTE,A.CODATENDIMENTO,E.NOMEPACIENTE PACIENTE,B.IDUNIDFAT,A.CODCOMPRADOR, A.SEQUENCIALCONTA FROM SZATENDIMENTO A INNER JOIN SZPARCIALATEND B ON  A.CODCOLIGADA = B.CODCOLIGADA     AND A.NUMEROCONTA = B.NUMEROCONTA  AND A.SEQUENCIALCONTA = B.SEQUENCIALCONTA INNER JOIN SZPACIENTE E ON  A.CODCOLIGADA = E.CODCOLIGADA  AND A.CODPACIENTE = E.CODPACIENTE INNER JOIN SZCADGERAL F ON A.CODCOLIGADA = F.CODCOLIGADA  AND B.CODCONVENIO = F.CODGERAL INNER JOIN SZFATURACONVENIO G ON A.CODCOLIGADA = G.CODCOLIGADA  AND B.CODCONVENIO = G.CODCOMPRADOR AND B.CODCOLIGADA = G.CODCOLIGADA  AND B.NUMEROREMESSA = G.NUMEROREMESSA WHERE A.CODCOLIGADA = 1 and  to_char(G.DATAGERACAO, 'MM/YYYY')= '"+str(valorCompetencia)+"' and g.numeroremessa = "+str(value)+" and g.codcomprador = "+str(codPlano)+" and B.IDUNIDFAT = "+str(codHospital)+" AND A.CODCOMPRADOR NOT IN (494, 25, 3087) UNION ALL SELECT 'CONTA DE RECURSO' TIPO_CONTA, F.SIGLA, A.DATAENTRADA, A.DATASAIDA, B.NUMEROREMESSA  REMESSA, to_char(G.DATAGERACAO, 'MM/YYYY') GERACAO, E.PRONTUARIO, E.CODPACIENTE, A.CODATENDIMENTO, E.NOMEPACIENTE  PACIENTE, B.IDUNIDFAT, A.CODCOMPRADOR, A.SEQUENCIALCONTA FROM SZATENDIMENTOREC A INNER JOIN SZPARCIALATEND B ON  A.CODCOLIGADA = B.CODCOLIGADA AND A.NUMEROCONTA = B.NUMEROCONTA AND A.SEQUENCIALCONTA = B.SEQUENCIALCONTA INNER JOIN SZPACIENTE E ON  A.CODCOLIGADA = E.CODCOLIGADA  AND A.CODPACIENTE = E.CODPACIENTE INNER JOIN SZCADGERAL F ON A.CODCOLIGADA = F.CODCOLIGADA  AND B.CODCONVENIO = F.CODGERAL INNER JOIN SZFATURACONVENIO G ON A.CODCOLIGADA = G.CODCOLIGADA  AND B.CODCONVENIO = G.CODCOMPRADOR  AND B.CODCOLIGADA = G.CODCOLIGADA  AND B.NUMEROREMESSA = G.NUMEROREMESSA WHERE A.CODCOLIGADA = 1 AND B.STATUS = 'F' and  to_char(G.DATAGERACAO, 'MM/YYYY')= '"+str(valorCompetencia)+"' and g.numeroremessa = "+str(value)+" and g.codcomprador = "+str(codPlano)+" and B.IDUNIDFAT = "+str(codHospital)+" AND G.STATUSPROTOCOLO = 'F' AND A.CODCOMPRADOR NOT IN (494, 25, 3087) ORDER BY PACIENTE)"
+        cursor.execute(sql2)
+        numeroTentativas = cursor.fetchone()[0]
+        print(numeroTentativas)
+        time.sleep(3)
+        if tipo_conta == "CONTA INICIAL":
+            def find_window_by_title(title):
+                handle = win32gui.FindWindow(None, title)
+                if handle == 0:
+                    print("Janela não encontrada.")
+                    return None
+                rect = win32gui.GetWindowRect(handle)
+                return rect
+            rect = find_window_by_title("Controle de Recebimentos (RDESKAMB.UNIFESO.LAN)")
+            if rect:
+                left, top, right, bottom = rect
+                print("Posição da janela: ({}, {}), ({}, {})".format(left, top, right, bottom))
+                screenshot1 = pyautogui.screenshot(region=(left, top, right-left, bottom-top))
+            for i in range(len(text['text'])):
+                if text['text'][i] == numeroConta:
+                    x, y, w, h = text['left'][i], text['top'][i], text['width'][i], text['height'][i]
+                    screen_x, screen_y = x + w / 2, y + h / 2
+                    py.doubleClick(screen_x, screen_y)
+                    break
+            else:
+                print("Texto não encontrado")
+                i = 0
+                while i < numeroTentativas:
+                    rolar = py.locateCenterOnScreen('itens/rolar.png')
+                    py.click(rolar)
+                    screenshot1 = pyautogui.screenshot()
+                    custom_config = r'--oem 1 --psm 11 --dpi 150 tessedit_char_whitelist=1234567890'
+                    text = ocr.image_to_data(screenshot1, output_type='dict',lang='por', config=custom_config)
+                    i += 1 
+                    continue
+
+        elif tipo_conta == "CONTA DE RECURSO":
+            contador += 1
+            continue
+
+        x, y = py.locateCenterOnScreen('itens/recebimento.png')
+        py.click(x, y)
+
+        data = py.locateCenterOnScreen('itens/data.png')
+        py.click(data)
+
+        valor = py.locateCenterOnScreen('itens/valor.png')
+        py.doubleClick(valor)
+        py.press('backspace')
+
+        perda = py.locateCenterOnScreen('itens/perda.png')
+        py.doubleClick(perda)
+        py.press('backspace')
+
+        #Algoritmo para achar a perda e passar para o ocr
+        screenshot = py.screenshot()
+        gray = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2GRAY)
+        item = cv2.imread("itens/valor_perda.png", 0)
+        result = cv2.matchTemplate(gray, item, cv2.TM_CCOEFF_NORMED)
+        _, _, _, loc = cv2.minMaxLoc(result)
+        x, y = loc[0], loc[1]
+        valorPerda = py.screenshot('testes.png', region=(x,y,65, 17))
+        image = cv2.imread('testes.png')
+        width = 97
+        height = 24
+        image = cv2.resize(image, (width, height))
+        image = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
+        image = cv2.addWeighted(image, 1.0, np.zeros(image.shape, image.dtype), 0, 50)
+        gray = cv2.Canny(image, 100, 200)
+        gray = cv2.Laplacian(image, cv2.CV_64F)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        #gray = cv2.convertScaleAbs(image, alpha=1.5, beta=0)
+        gray = cv2.bilateralFilter(gray,9,75,75)
+        gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        gray = cv2.equalizeHist(gray)
+        gray = cv2.GaussianBlur(gray, (1,1), 0)
+        thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        custom_config = r'--oem 1 --psm 11 tessedit_char_whitelist=1234567890'
+        phrase1 = ocr.image_to_string(thresh, lang='por', config=custom_config)
+        phrase1 = phrase1.replace(" ", "")
+        locale.setlocale(locale.LC_ALL, "Portuguese_Brazil.1252")
+        valor = locale.atof(phrase1)
+        valor_string = str(valor)
+        print(valor_string)
+        py.write(valor_string.replace(".", ","), interval=0.3)
+
+        gravar = py.locateCenterOnScreen('itens/gravar.png')
+        py.click(gravar)        
+        py.write('1')
+
+        py.press('tab')
+        py.press('tab')
+        py.press('tab')
+        py.press('enter')
+        title = "Erro (RDESKAMB.UNIFESO.LAN)"
+        time.sleep(10)
+        py.press('enter')
+        #ok = py.locateCenterOnScreen('itens/ok.png')
+        #py.click(ok)
+        cancelar = py.locateCenterOnScreen('itens/cancelar.png')
+        py.click(cancelar)
+        contador += 1
+
 
 def recuperarLista():
     if Checkbutton1.get():
